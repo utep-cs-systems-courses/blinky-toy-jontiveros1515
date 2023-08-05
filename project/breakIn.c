74f50da31e7767c8ab2063ef3ac21bfb1d8f3567
Javier Ontiveros <50270099+jontiveros1515@users.noreply.github.com>
2023-08-05 04:29:23 -0600

    adding changes

---
+++ project/breakIn.c
@@ -0,0 +1,24 @@
+//It counts the change
+#include <msp430.h>
+
+#include "stateMachines.h"
+
+
+
+
+
+void
+
+__interrupt_vec(WDT_VECTOR) WDT(){
+
+  static char count = 0;
+
+  if(++count == 20){
+
+    state_advance();
+
+    count = 0;
+
+  }
+
+}
---
+++ project/stateMachines.h
@@ -0,0 +1,12 @@
+#ifndef stateMachine_included
+
+#define stateMachine_included
+
+
+
+void state_advance();
+
+char greenOn();
+
+#endif 
+//It turns the color green on
