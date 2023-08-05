e89ebe8ce3bd86c0a8eccd7088cc71d225864825
Javier Ontiveros <50270099+jontiveros1515@users.noreply.github.com>
2023-08-05 05:33:44 -0600

    Delete project directory

--- project/toy/buzzerStateMachine.h
+++
@@ -1,9 +0,0 @@
-#ifndef buzzerStateMachine_included
-#define buzzerStateMachine_included
-
-extern int secondCount;
-extern int state;
-
-void buzz();
-
-#endif
--- project/toy/stateMachine.h
+++
@@ -1,7 +0,0 @@
-#ifndef led_included
-#define led_included
-
-void updateState(int state);
-
-#endif
-
